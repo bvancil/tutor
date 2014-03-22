387cffc7896966951afde1d2563cb6486aa28afd
Brian Vancil <Brian@Vancil.name>
2014-03-21 23:49:35 -0500

    Discover Meteor ch 7

--- client/views/posts/post_submit.js
+++ client/views/posts/post_submit.js
@@ -1,14 +1,17 @@
 Template.postSubmit.events({
   'submit form': function(e) {
-    e.preventDefault();
-
-    var post = {
-      url: $(e.target).find('[name=url]').val(),
-      title: $(e.target).find('[name=title]').val(),
-      message: $(e.target).find('[name=message]').val()
-    }
-
-    post._id = Posts.insert(post);
-    Router.go('postPage', post);
+		e.preventDefault();
+		
+		var post = {
+	    url: $(e.target).find('[name=url]').val(),
+	    title: $(e.target).find('[name=title]').val(),
+	    message: $(e.target).find('[name=message]').val()
+		}
+		Meteor.call('post', post, function(error, id) {
+	    if (error)
+				return alert(error.reason);
+	    
+	    Router.go('postPage', {_id: id});
+		});
   }
 });
